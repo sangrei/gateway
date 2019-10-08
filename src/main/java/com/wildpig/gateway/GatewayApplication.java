db09a92298732ef0b29630aabb4fa51b3b187195
sangrei <sangrei@163.com>
2019-10-08 22:51:22 +0800

    修改成使用zuul

--- src/main/java/com/wildpig/gateway/GatewayApplication.java
+++ src/main/java/com/wildpig/gateway/GatewayApplication.java
@@ -3,9 +3,11 @@ package com.wildpig.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 //@EnableOAuth2Sso
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class GatewayApplication {
